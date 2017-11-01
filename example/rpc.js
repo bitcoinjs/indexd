bf49b6ace14857dd5f340a7b4b55f0d417f504f9
Daniel Cousens <github@dcousens.com>
2017-11-01 12:07:44 +1100

    rpc: use cookie auth

--- example/rpc.js
+++ example/rpc.js
@@ -1,7 +1,6 @@
 module.exports = require('yajrpc/qup')({
   url: process.env.RPC,
-  user: process.env.RPCUSER,
-  pass: process.env.RPCPASSWORD,
-  batch: process.env.RPCBATCHSIZE,
-  concurrent: process.env.RPCCONCURRENT
+  auth: require('fs').readFileSync(process.env.RPCCOOKIE),
+  batch: process.env.RPCBATCHSIZE || 500,
+  concurrent: process.env.RPCCONCURRENT || 32
 })
