39d1538444162fe9f01108fb16da752be31c8450
Daniel Cousens <github@dcousens.com>
2017-11-02 12:14:56 +1100

    example: default to 16 concurrent for new users

--- example/rpc.js
+++ example/rpc.js
@@ -2,5 +2,5 @@ module.exports = require('yajrpc/qup')({
   url: process.env.RPC,
   auth: require('fs').readFileSync(process.env.RPCCOOKIE),
   batch: process.env.RPCBATCHSIZE || 500,
-  concurrent: process.env.RPCCONCURRENT || 32
+  concurrent: process.env.RPCCONCURRENT || 16
 })
