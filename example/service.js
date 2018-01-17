a4d57156f8173d63b748f8df69903520fd54f5d1
Daniel Cousens <github@dcousens.com>
2018-01-17 16:14:11 +1100

    poll every 60s too

--- example/service.js
+++ example/service.js
@@ -1,9 +1,9 @@
 let debug = require('debug')('example:service')
-let debugZMQ = require('debug')('service:zmq')
+let debugZmq = require('debug')('service:zmq')
+let debugZmqTx = require('debug')('service:zmq:tx')
 // let indexd = require('indexd') // XXX: you should use npm published package typically
 let indexd = require('../')
 let leveldown = require('leveldown')
-let qup = require('qup')
 let rpc = require('./rpc')
 let zmq = require('zmq')
 
@@ -15,59 +15,62 @@ module.exports = function initialize (callback) {
     if (err) debug(err)
   }
 
+  let syncing = false
+  function resync () {
+    // already in progress?
+    if (syncing) return
+    syncing = true
+
+    indexd.resync(rpc, adapter, (err) => {
+      syncing = false
+      errorSink(err)
+      adapter.mempool.reset(errorSink)
+    })
+  }
+
   debug(`Opening leveldb @ ${process.env.INDEXDB}`)
   db.open({
-    writeBufferSize: 1 * 1024 * 1024 * 1024
+    writeBufferSize: 1 * 1024 * 1024 * 1024 // 1 GiB
   }, (err) => {
     if (err) return callback(err)
     debug(`Opened leveldb @ ${process.env.INDEXDB}`)
-
-    let syncQueue = qup((_, next) => {
-      indexd.resync(rpc, adapter, (err) => {
-        if (err) return next(err)
-        adapter.mempool.reset(next)
-      })
-    }, 1)
 
     let zmqSock = zmq.socket('sub')
     zmqSock.connect(process.env.ZMQ)
     zmqSock.subscribe('hashblock')
     zmqSock.subscribe('hashtx')
 
-    let lastSequence
+    let lastSequence = 0
     zmqSock.on('message', (topic, message, sequence) => {
       topic = topic.toString('utf8')
       message = message.toString('hex')
       sequence = sequence.readUInt32LE()
 
-      // if any ZMQ messages were lost,  assume a resync is required
+      // were any ZMQ messages were lost?
       let expectedSequence = lastSequence + 1
-      if (Number.isFinite(expectedSequence) && sequence !== expectedSequence) {
-        if (sequence < expectedSequence) {
-          debugZMQ(`bitcoind may have restarted`)
-        } else {
-          debugZMQ(`${sequence - expectedSequence} messages lost`)
+      lastSequence = sequence
+      if (sequence !== expectedSequence) {
+        if (sequence < expectedSequence) debugZmq(`bitcoind may have restarted`)
+        else debugZmq(`${sequence - expectedSequence} messages lost`)
+        resync()
+      }
+
+      switch (topic) {
+        case 'hashblock': {
+          debugZmq(topic, message)
+          return resync()
         }
 
-        lastSequence = sequence
-        return syncQueue.push(null, errorSink)
+        case 'hashtx': {
+          debugZmqTx(topic, message)
+          return adapter.mempool.add(message, errorSink)
+        }
       }
-
-      lastSequence = sequence
-      if (topic === 'hashblock') {
-        debugZMQ(topic, message)
-        return syncQueue.push(null, errorSink)
-      }
-
-      // don't add to the mempool until after a reset is complete
-      if (syncQueue.running > 0) return
-      if (topic !== 'hashtx') return
-      debugZMQ(topic, message)
-
-      adapter.mempool.add(message, errorSink)
     })
 
-    syncQueue.push(null, errorSink)
+    setInterval(resync, 60000) // attempt every minute
+    resync()
+
     callback()
   })
 }
